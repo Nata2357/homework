babe16393290faaf45c1ae70323efd99f4de43fc
Nata2357 <114216514+Nata2357@users.noreply.github.com>
2022-11-17 10:39:33 +0700

    68

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Задайте начальное число M:");
+int numberM = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Задайте начальное число N:");
+int numberN = int.Parse(Console.ReadLine());
+
+int AckermannFunction (int numberM, int numberN)
+{
+    if (numberM == 0) return numberN + 1;
+    if (numberM != 0 && numberN == 0) return AckermannFunction(numberM - 1, 1);
+    if (numberM > 0 && numberN > 0) return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
+return AckermannFunction(numberM, numberN);
+}
+
+Console.WriteLine($"Функция Аккермана для чисел A({numberM},{numberN}) = {AckermannFunction(numberM, numberN)}");
